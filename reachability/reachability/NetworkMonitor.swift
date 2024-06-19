ade525d980a1a2ca0e96a13468fb42dfa8569f71
Dulio Denis <dulio.denis@yahoo.com>
2024-06-19 19:02:29 -0400

    Added NetworkMonitor class

--- reachability/reachability/NetworkMonitor.swift
+++ reachability/reachability/NetworkMonitor.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import Network
+
+@MainActor
+class NetworkMonitor: ObservableObject {
+    static let shared = NetworkMonitor()
+    
+    private var monitor: NWPathMonitor
+    
+    @Published var isConnected: Bool = false
+    @Published var connectionType: ConnectionType = .unknown
+    
+    enum ConnectionType {
+        case wifi
+        case cellular
+        case ethernet
+        case unknown
+    }
+    
+    private init() {
+        monitor = NWPathMonitor()
+    }
+    
+    func startMonitoring() {
+        monitor.pathUpdateHandler = { path in
+            Task { @MainActor in
+                self.isConnected = path.status == .satisfied
+                self.determineConnectionType(path)
+            }
+        }
+        monitor.start(queue: .global(qos: .background))
+    }
+    
+    private func determineConnectionType(_ path: NWPath) {
+        if path.usesInterfaceType(.wifi) {
+            connectionType = .wifi
+        } else if path.usesInterfaceType(.cellular) {
+            connectionType = .cellular
+        } else if path.usesInterfaceType(.wiredEthernet) {
+            connectionType = .ethernet
+        } else {
+            connectionType = .unknown
+        }
+    }
+}
